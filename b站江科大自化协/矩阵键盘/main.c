b8bbe90ec8087e6dde5f6fd9a928f6db7ffbbe6c
good good study <62123987+xuexi6666@users.noreply.github.com>
2022-03-14 10:48:35 +0800

    Add files via upload

---
+++ b站江科大自化协/矩阵键盘/main.c
@@ -0,0 +1,17 @@
+#include <REGX52.H>
+#include "LCD1602.h"
+#include "Delay.h"
+
+//int reasult=0;
+
+//void main()
+//{
+//	LCD_Init();
+//	while(1)
+//	{
+//	reasult++;
+//	Delay(300);
+//	LCD_ShowNum(2,2,reasult,3);		//显示读秒器
+//	LCD_ShowString(1,1,"hello world");显示字符
+//	}
+//}
